1b99c2b06916a891b96f3b85c683cea9f770c14c
Yazan Barhoush <ybarhoush@gmail.com>
2017-01-22 15:52:25 -0500

    Handles Color changing, created ColorCmd.java, set and get color methods were added to Drawing.java --AddShape classes were modified to handle color change

---
+++ Project1-Code/src/ColorCmd.java
@@ -0,0 +1,19 @@
+/**
+ * Created by fedora on 1/22/17.
+ */
+import java.awt.*;
+
+public class ColorCmd extends Command {
+
+    /**
+     * change Shape color to the Drawing object's current set color
+     */
+    public void executeClick(Point p, Drawing dwg) {
+        Shape clickedShape = dwg.getFrontmostContainer(p);
+
+        if (clickedShape != null)
+        {
+            clickedShape.setColor(dwg.getColor());
+        }
+    }
+}
